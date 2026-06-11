c1b384bbd49ff9dffb0a4a86289097f660d66ed1
IJbeasley <isobel.beasley@ucsf.edu>
2026-06-11 15:16:27 -0700

    Cleaning up population description mapping code

---
+++ code/pop_desc_processing/gwas_cat_metadata.R
@@ -0,0 +1,448 @@
+# Functions for extracting and grouping
+# population descriptors in GWAS Catalog
+# sample description metadata
+
+get_ancestry_from_sample_desc = function(sample_description){
+  # Possible Ancestry/Description Terms in Sample Description Column of
+  # GWAS Catalog Ancestry Metadata
+
+  # sorted in groups of similar terms,
+  # roughly by length of matched terms
+  multi_group_ancestry_terms <- c(
+
+    # Start with many groups
+    'European, African, African American, South East Asian, East Asian, American Indian\\/Alaskan Native, Mixed race, Arabic\\/North African, Central\\/South Asian or Japanese Asian',
+    '^European, African American, Hispanic, Asian, Pacific Islander and other(?= ancestry)',
+    'European ancestry, African American or Afro-Caribbean, Hispanic or Latin American',
+
+    'Asian, European, African and other(?= ancestry)',
+    'European, African, Asian and unknown(?= ancestry)',
+    'South Asian, Afro-Caribbean or unknown(?= ancestry)',
+    'European, African American, South Asian and Hispanic',
+
+    # European and/or African / African American
+    '(?<![Ff]innish )European and African(?= ancestry)',
+    '(?<![Ff]innish )European or African American',
+
+    # European and Asian
+    "European, South Asian or East Asian",
+    'European, Hispanic and other(?= ancestry)',
+    'European ancestry, East Asian ancestry, unknown',
+    '(?<![Ff]innish )European and South Asian(?= ancestry)',
+    "(?<!Han )Chinese or European(?= ancestry)",
+    '(?<![Ff]innish )European or East Asian(?= ancestry)',
+    '(?<![Ff]innish )European and East Asian(?= ancestry)',
+
+    # European and unknown ancestry
+    '(?<![Ff]innish )European ancestry, unknown(?= cases)',
+    '(?<![Ff]innish )European or unknown(?= ancestry)',
+    '(?<![Ff]innish )European and unknown(?= ancestry)',
+    '(?<![Ff]innish )European or uknown(?= ancestry)',
+
+    # European and other
+    'Ashkenazi Jewish',
+    '(?<![Ff]innish )European and other(?= ancestry)',
+    '(?<![Ff]innish )European ance other(?= ancestry)',
+
+
+    'African and Asian(?= ancestry)',
+    'Sub-Saharan African ancestry and African ancestry',
+    'East Asian, Asian, and other(?= ancestry)'
+  )
+
+  # sorted in groups of similar terms,
+  # roughly by length of matched terms
+  single_ancestry_terms = c(
+    # 'African American or Afro-Caribbean, African etc.'
+    'African(?:-|\\s)American or Afro-Caribbean',
+    'African or African(?:-|\\s)American', # African or African American
+    'African American, African(?= ancestry)',
+    '(?<!or )African(?:-|\\s)American(?! or Afr)',
+    '(?<!or )Afro-Caribbean(?! or)',
+    'African unspecified',
+    '(?<!South )(?<!North )(?<!West )(?<!Sub-Saharan )African(?!-|\\s)(?! or)',
+    'South African(?! or)',
+    'West African(?! or)',
+    'Sub-Saharan African(?! or)(?! and)',
+    'Jamaican',
+    'Ethiopian',
+    'Black or Black British',
+    'Black(?! or)(?! British)',
+
+    # Oceanian
+    'Fijian Indian',
+    'Filipino(?= ancestry)',
+    'Oceanian',
+    'Martu Australian Aboriginal(?= ancestry)',
+    'Aboriginal Australian',
+
+    # American Native, Latino, American Admixed
+    'American Indian\\/Alaskan Native',
+    'American Indian(?!\\/)',
+    '(?<!Indigenous )Mexican and other Latin American',
+    'Indigenous Mexican(?! and)(?! or)',
+    'Mexican American',
+    'Admixed American',
+    '(?<! Indigenous )Mexican(?! American)(?! and)',
+    'Hispanic\\/Latino',
+    'Hispanic American',
+    'Hispanic or Latin American',
+    'Hispanic or Latino',
+    'Hispanic(?! or Latin)(?! American)(?!/)',
+    '(?<! other)(?<!or )Latin American',
+    '(?<!\\% )Native American',
+    '(?<!\\% )Native Hawaiian',
+    'Pima Indian',
+    'Costa Rican',
+
+
+    # Asian, East Asian etc.
+    'Asian or Asian British',
+    'Central/South Asian(?! unspecified)',
+    'Central Asian(?! unspecified)',
+    'South East Asian',
+    '(?<!South )East Asian',
+    "(?<!East )(?<!South )(?<!Central )Asian(?! unspecified)(?!or)",
+    "(?<!East )(?<!South )(?<!Central )Asian(?= unspecified)",
+    'Han(?:-|\\s)Chinese',
+    "(?<!Han )Southern Chinese", # but not Han Chinese
+    '(?<!Han )(?<!Southern )Chinese',
+    'Japanese American',
+    'Japanese(?! American)',
+    'Oriental(?= ancestry)',
+    'Pakistani(?= ancestry)',
+    '(?<![Ww]hite )(?<![Bb]lack )(?<![Aa]sian )British',
+    '(?<!American )(?<!Fijian )Northern Indian(?! Asian)',
+    'Indian Asian',
+    'Asian Indian',
+    '(?<!American )(?<!Fijian )(?<!Northern )(?<!Pima )Indian(?! Asian)',
+    'Korean',
+    'South Asian(?! or)(?! and)',
+    'Malaysian',
+    'Malay\\b',
+    'Taiwanese',
+
+    # Middle eastern
+    'Greater Middle Eastern \\(Middle Eastern, North African or Persian\\)',
+    '(?<!Greater )(?<!\\()Middle Eastern(?! \\()',
+    'Greater Middle Eastern(?= ancestry)',
+    'Saudi Arabian',
+    'Saudi Arab(?= ancestry)',
+    'Lebanese',
+    'Arabic\\/North African(?= ancestry)',
+    'Arab\\b',
+
+    # European ancestry
+    'Findland founder',
+    'Finnland founder',
+    '[Nn]on-[Ff]innish European',
+    '(?<![Nn]on-)[Ff]innish',
+    'French [Cc]andian',
+    'French(?= ancestry)',
+    '(?<![Ff]innish )(?<!with )European',
+    'Greek(?= ancestry)',
+    'Old Order Amish',
+    '(?<!Old Order )Amish',
+    'Celtic',
+    'German',
+    'Italian',
+    'Mylopotamos',
+    'Polish(?= ancestry)',
+    'Scottish',
+    'Spanish',
+    'Slavic',
+    'Swedish',
+
+    # Other, unknown, or admixed unspecified
+    '(?<!and )[Oo]ther(?:-|\\s)[Aa]dmixed(?= ancestry)',
+    'mixed and [Oo]ther(?= ancestry)',
+    '[Oo]ther(?= ancestry)',
+    'NR',
+    'unknown',
+    "White(?= ancestry)",
+    'Mixed race'
+  )
+
+  ancestry_terms <-   c(
+    multi_group_ancestry_terms,
+    single_ancestry_terms
+  )
+
+  # try match one term at a time
+  vapply(sample_description, function(desc){
+    if (is.na(desc) || !nzchar(desc)) return(NA_character_)
+
+    remaining <- desc
+    found <- character(0)
+
+    for (term in ancestry_terms){
+
+      hit <- stringr::str_extract(remaining,
+                                  term)
+      if (!is.na(hit)){
+        found <- c(found, hit)
+
+        remaining <- stringr::str_remove_all(remaining,
+                                             term)
+      }
+    }
+
+    if (length(found) == 0) return('NR')
+
+    found <- paste(found,
+                  collapse = "; ")
+
+    return(found)
+    # return in the order the terms appear in the original description
+    #   found <- found[order(vapply(found, function(t)
+    #     str_locate(desc, t)[, "start"], numeric(1)))]
+    #   paste(found, collapse = ", ")
+    # }, character(1), USE.NAMES = FALSE)
+  },
+  character(1),
+  USE.NAMES = FALSE)
+}
+
+
+
+# code adapted from https://github.com/armartin/prs_disparities/blob/master/gwas_disparities_time.R
+group_pop_labels <- function(study_desc) {
+
+  study_desc <- stringr::str_to_title(study_desc)
+
+  pop_lab <- dplyr::case_when(
+
+    #################### African American or Afro-Caribbean + other ancestry groups ###############
+
+    # "African American or Afro-Caribbean, African unspecified"
+    study_desc %in% c('African, African American',
+                      'African American, African',
+                      'African or African American'
+    ) ~ "African American or Afro-Caribbean, African unspecified",
+
+    #  African American or Afro-Caribbean, Asian unspecified, European, Hispanic or Latin American, Oceanian, Other
+    study_desc %in% c('European, African American, Hispanic, Asian, Pacific Islander And Other') ~
+      'African American or Afro-Caribbean, Asian unspecified, European, Hispanic or Latin American, Oceanian, Other',
+
+    # African American or Afro-Caribbean, NR, South Asian
+    study_desc %in% c('South Asian, Afro-Caribbean Or Unknown') ~
+      'African American or Afro-Caribbean, NR, South Asian',
+
+    # "African American or Afro-Caribbean, European"
+    study_desc %in% c('European or African American') ~
+      "African American or Afro-Caribbean, European",
+
+    # "African American or Afro-Caribbean, European, Hispanic or Latin American"
+    study_desc %in% c('European, African American, South Asian And Hispanic',
+                      'European ancestry, African American or Afro-Caribbean, Hispanic or Latin American') ~
+      "African American or Afro-Caribbean, European, Hispanic or Latin American",
+
+
+    #################### African unspecified + other ancestry groups ###############
+
+    # "African unspecified, Asian unspecified"
+    study_desc %in% c('African And Asian') ~
+      'African unspecified, Asian unspecified',
+
+    # "African unspecified, Asian unspecified, European, NR"
+    study_desc %in% c('Asian, European, African And Other',
+                      'European, African, Asian And Unknown') ~
+      "African unspecified, Asian unspecified, European, NR",
+
+    # African unspecified, European
+    study_desc %in% c('European And African') ~
+      'African unspecified, European',
+
+    # African unspecified, Sub-Saharan African
+    study_desc %in% c('Sub-Saharan African Ancestry And African Ancestry') ~
+      "African unspecified, Sub-Saharan African",
+
+    #################### Asian unspecified + other ancestry groups ###############
+
+    # Asian unspecified, East Asian, Other
+    study_desc %in% c('East Asian, Asian, And Other') ~
+      "Asian unspecified, East Asian, Other",
+
+    #################### Central Asian + other ancestry groups ###############
+
+    # "Central Asian, South Asian"
+    study_desc %in% c('Central/South Asian') ~
+      "Central Asian, South Asian",
+
+    #################### East Asian + other ancestry groups ###############
+
+    # "East Asian, European"
+    study_desc %in% c('Chinese Or European',
+                      'European And East Asian',
+                      'European Or East Asian') ~  "East Asian, European",
+
+    # "East Asian, European, NR"
+    study_desc %in% c('European Ancestry, East Asian Ancestry, Unknown') ~
+      "East Asian, European, NR",
+
+    # "East Asian, European, South Asian"
+    study_desc %in% c('European, South Asian Or East Asian') ~
+      "East Asian, European, South Asian",
+
+
+    #################### European + other ancestry groups ###############
+
+    # European, NR
+    study_desc %in% c('European Or Unknown',
+                      'European And Unknown',
+                      'European Or Uknown',
+                      'European Ancestry, Unknown') ~
+      'European, NR',
+
+    # European, Other
+    study_desc %in% c('European Ance Other',
+                      'European And Other') ~
+      'European, Other',
+
+    # European, South Asian
+    study_desc %in% c('European And South Asian') ~
+      'European, South Asian',
+
+    # European, Hispanic or Latin American, Other
+    study_desc %in% c('European, Hispanic And Other') ~
+      'European, Hispanic or Latin American, Other',
+
+
+    ############### Single broad ancestry groups ################
+
+    # African-American or Afro-Caribbean
+    study_desc %in% c('Afro-Caribbean',
+                      'Afro Caribbean',
+                      'African American',
+                      'African-American',
+                      'African American Or Afro-Caribbean',
+                      'African-American Or Afro-Caribbean',
+                      'African American Or Afro Caribbean',
+                      "Jamaican")
+    ~ "African-American or Afro-Caribbean",
+
+    # African unspecified
+    study_desc %in% c('African',
+                      'African Unspecified') ~
+      "African unspecified",
+
+    # Asian unspecified
+    study_desc %in% c('Asian',
+                      'Oriental',
+                      'Asian Unspecified') ~ "Asian unspecified",
+
+    # Central asian
+    study_desc %in% c('Central Asian') ~
+      "Central Asian",
+
+    # East Asian
+    study_desc %in% c("Japan",
+                      "Japanese",
+                      "Korea",
+                      "Korean",
+                      "Chinese",
+                      "Han-Chinese",
+                      'Han Chinese',
+                      "Taiwan",
+                      'Taiwanese',
+                      "East Asian"
+    ) ~ "East Asian",
+
+    # European
+    study_desc %in% c('Amish',
+                      'British',
+                      'Celtic',
+                      'European',
+                      'Non-Finnish European',
+                      'Finnish',
+                      'Findland founder',
+                      'Finnland founder'
+                      'French Canadian',
+                      'French',
+                      'Greek',
+                      'German',
+                      'Italian',
+                      'Mylopotamos',
+                      'Old Order Amish',
+                      'Polish',
+                      'Scottish'
+                      'Slavic',
+                      'Spanish',
+                      'Swedish',
+                      'White'
+                      ) ~ 'European',
+
+    #  Greater Middle Eastern
+    study_desc %in% c('Greater Middle Eastern (Middle Eastern, North African or Persian)',
+                      'North African',
+                      'Saudi Arab',
+                      'Saudi Arabian',
+                      'Lebanese',
+                      'Persian',
+                      'Middle Eastern',
+                      'Arab') ~ 'Greater Middle Eastern (Middle Eastern, North African or Persian)',
+
+    # Sub-Saharan African
+    study_desc %in% c('South African',
+                      'West African',
+                      'Sub-Saharan African',
+                      'Ethiopian') ~ "Sub-Saharan African",
+
+    # South Asian
+    study_desc %in% c('Indian',
+                      'Indian Asian',
+                      'Asian Indian',
+                      'Pakistani',
+                      'South Asian') ~ "South Asian",
+
+
+    # South East Asian
+    study_desc %in% c('South East Asian',
+                      'Malaysian',
+                      'Malay',
+                      'Vietnam',
+                      'Vietnamese') ~ "South East Asian",
+
+
+    # Hispanic or Latin American
+    study_desc %in% c('Hispanic',
+                      'Hispanic/Latin American',
+                      'Latin American/Hispanic',
+                      'Hispanic/Latino',
+                      'Hispanic American',
+                      'Mexican',
+                      'Mexican American',
+                      'Costa Rican',
+                      'Hispanic Or Latin American',
+                      'Latin American') ~ 'Hispanic or Latin American',
+
+    # Native American
+    study_desc %in% c("Native American",
+                      "Alaskan Native",
+                      'American Indian'
+    ) ~ "Native American",
+
+    # Not reported
+    study_desc %in% c("NR",
+                      "Unknown") ~ "NR",
+
+    # Oceanian
+    study_desc %in% c("Native Hawaiian",
+                      'Pacific Islander'
+    ) ~ "Oceanian",
+
+    # Other
+    study_desc %in% c('Other') ~ "Other",
+
+    # Other Admixed
+    study_desc %in% c('Other-Admixed',
+                      'Admixed American',
+                      'Other Admixed') ~
+      "Other admixed ancestry"
+
+    TRUE~"Nothing"
+  )
+
+  return(pop_lab)
+}
+
---
+++ code/pop_desc_processing/plot_gwas_ancest_comp.R
@@ -0,0 +1,178 @@
+# Functions for simplifying broad ancestry groups in
+# GWAS Catalog Metadata
+# And for estimating + plotting ancestry composition of
+# GWAS Catalog samples
+
+ancestry_colors <- c(
+  "African" = "#984EA3",
+  "European" = "#E41A1C",
+  "Asian" = "#377EB8", #east asian
+  # "South Asian" = "#4DAF4A",
+  "Hispanic/Latin American" = "#FF7F00",
+  "Middle Eastern" = "#FFFF33",
+  "Oceanic" = "#A65628",
+  "Other" = "#F781BF",
+  "Multiple" = "#999999",
+  "Not reported" = "black"
+)
+
+
+# Define the desired stacking order
+ancestry_levels <- c(
+  "European",
+  # "East Asian",
+  # "South Asian",
+  "Asian",
+  "African",
+  "Hispanic/Latin American",
+  "Middle Eastern",
+  "Oceanic",
+  "Other",
+  "Multiple",
+  "Not reported"
+)
+
+
+group_ancestry_fn = function(study_ancest){
+
+  case_when(
+
+    # European
+    study_ancest %in% c('European') ~ 'European',
+
+    # African
+    study_ancest %in% c('Sub-Saharan African, African American or Afro-Caribbean',
+                        'African unspecified, African American or Afro-Caribbean',
+                        'African American or Afro-Caribbean, African unspecified',
+                        'Sub-Saharan African, African unspecified',
+                        'African-American or Afro-Caribbean',
+                        'Sub-Saharan African',
+                        'African American or Afro-Caribbean',
+                        'African unspecified') ~ 'African',
+
+    # Asian
+    study_ancest %in% c('East Asian, Asian unspecified',
+                        'South Asian, East Asian ',
+                        'South Asian, South East Asian',
+                        'South Asian, South East Asian, East Asian',
+                        'South East Asian, East Asian',
+                        'South East Asian, South Asian, East Asian',
+                        'South Asian, South East Asian, East Asian, Asian unspecified',
+                        'South East Asian, East Asian, South Asian',
+                        'East Asian, South Asian, South East Asian',
+                        'East Asian, South East Asian, South Asian, Asian unspecified',
+                        'South Asian',
+                        'South East Asian',
+                        'South Asian, East Asian',
+                        'South Asian, Asian unspecified',
+                        'Central Asian, South Asian',
+                        'East Asian, South Asian',
+                        'Central Asian',
+                        'East Asian',
+                        'Asian unspecified') ~'Asian',
+
+    # Middle eastern
+    study_ancest %in% c('Greater Middle Eastern (Middle Eastern, North African or Persian)',
+                        'Middle Eastern') ~'Middle Eastern',
+
+    # Oceanic
+    study_ancest %in% c('Aboriginal Australian',
+                        'Oceanian') ~'Oceanic',
+
+    # Hispanic/Latin American
+    study_ancest %in% "Hispanic or Latin American" ~'Hispanic/Latin American',
+
+    # Other
+    study_ancest %in% c('Other',
+                        'Other, NR',
+                        'NR, Other',
+                        'other',
+                        'Native American') ~ "Other",
+
+    # Not reported
+    study_ancest %in% c("NR") ~ "Not reported",
+
+    # Multiple
+    grepl(", ", study_ancest) ~ 'Multiple',
+
+    TRUE~study_ancest
+  )
+
+
+
+}
+
+
+# Set up custom theme for ggplots
+custom_theme <-
+  list(
+    theme_bw(base_size = 20) +
+      theme(
+        panel.border = element_blank(),
+        axis.line = element_line(),
+        #text = element_text(size = 16),
+        legend.position = "right",
+        strip.background = element_blank()
+        #axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)
+      )
+  )
+
+
+# Add final totals
+add_final_totals <- function(ancestry_df){
+
+  max_date = ancestry_df$DATE |> max()
+
+  ancestry_df_totals= ancestry_df |>
+    dplyr::group_by(ancestry_group) |>
+    dplyr::summarise(ancest_cumsum = max(ancest_cumsum), .groups = "drop") |>
+    dplyr::mutate(DATE = max_date)
+
+  ancestry_df = bind_rows(ancestry_df, ancestry_df_totals)
+
+  return(ancestry_df)
+
+
+
+}
+
+# Add final totals, for reversed axis:
+add_final_totals_reversed <- function(ancestry_df){
+
+  min_date = ancestry_df |>
+             ungroup() |>
+             pull(DATE) |>
+             min()
+
+  ancestry_df_totals = ancestry_df |>
+    dplyr::group_by(ancestry_group) |>
+    slice_max(order_by = ancest_cumsum, n = 1, with_ties = F) |>
+    #dplyr::summarise(ancest_cumsum = max(ancest_cumsum), .groups = "drop") |>
+    dplyr::mutate(DATE = min_date) |>
+    dplyr::ungroup()
+
+  ancestry_df = bind_rows(ancestry_df,
+                          ancestry_df_totals)
+
+  max_date = ancestry_df |>
+    ungroup() |>
+    pull(DATE) |>
+    max()
+
+  #max_date = lubridate::rollforward(max_date)
+
+  # add a O tally
+  start_tally =
+  ancestry_df |>
+    select(ancestry_group) |>
+    distinct() |>
+    mutate(ancest_cumsum = 0,
+           DATE = max_date)
+
+  ancestry_df = bind_rows(start_tally,
+                          ancestry_df)
+
+
+  return(ancestry_df)
+
+}
